25e0a8bb6e97e6e35e842372e0cfca93afb773cb
CharweeH <CharlotteGov@gmail.com>
2025-05-05 20:39:35 +0100

    added emojis for various pets

--- bot.py
+++ bot.py
@@ -37,12 +37,45 @@ async def hello_world(ctx):
 
 bot.run(TOKEN)
 
-#Storing user's pet data
-
 DATA_FILE = pet_data.json
 
+PET_EMOJIS = {
+    "dog" = "🐶",
+    "cat" = "🐱",
+    "rabbit" = "🐰",
+    "parrot" = "🦜",
+    "snake" = "🐍",
+    "monkey" = "🐒",
+    "penguin" = "🐧".
+    "panda" = "🐼",
+    "hamster" = "🐹",
+    "raccoon" = "🦝",
+    "cow" = "🐮",
+    "seal" = "🦭",
+    "dinosaur" = "🦖",
+    "unicorn" = "🦄",
+    "default" = "🐾"
+}
+
+#Storing user's pet data
+
 def load_pet_data():
-    if os.
+    if os.path.exists(DATA_FILE)
+      with open(DATA_FILE, "r") as f:
+          return.json(f)
+    
+    else:
+        return {}
+    
+def save_pet_data(data):
+    with open(DATA_FILE, "w") as f:
+        json.dump(data, f, indent=4)
+
+def get_pet_emoji(pet_type):
+    return PET_EMOJIS.get(pet_type.lower(), PET_EMOJIS["default"])
+
+
+
 
 
 
